b0b5b36626add34728dab12816a7fc9c192575f0
jaime_hernandez <jahernandezsa@falabella.cl>
2024-07-05 17:43:47 -0400

    change

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,16 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://codeiaspa.github.io",
-  integrations: [tailwind(), mdx(), sitemap(), icon()],
+  integrations: [tailwind(), mdx(), sitemap(), icon(
+    {
+      include: {
+        mdi: ['whatsapp'],
+      }
+    }
+  ), react()],
 });
